0c00821ab6a1ef7abaa60382df4bf86e54b9be63
josselinonduty <contact@josselinonduty.fr>
2025-02-02 10:17:03 +0100

    feat: use cors and authorization guards to api endpoints

--- server/api/health.get.ts
+++ server/api/health.get.ts
@@ -1,14 +1,19 @@
-export default defineEventHandler(async (event) => {
-  setResponseStatus(event, 200);
-  const config = useRuntimeConfig(event);
-  const redis = await getRedis();
+export default defineEventHandler({
+  onRequest: [cors()],
+  handler: async (event) => {
+    setResponseStatus(event, 200);
+    const config = useRuntimeConfig(event);
+    const redis = await getRedis();
 
-  return {
-    apiVersion: config.apiVersion,
+    return {
+      apiVersion: config.apiVersion,
 
-    messageBroker: {
-      pub: redis?.pub && redis.pub.isOpen && redis.pub.isReady ? "ok" : "error",
-      sub: redis?.sub && redis.sub.isOpen && redis.sub.isReady ? "ok" : "error",
-    },
-  };
+      messageBroker: {
+        pub:
+          redis?.pub && redis.pub.isOpen && redis.pub.isReady ? "ok" : "error",
+        sub:
+          redis?.sub && redis.sub.isOpen && redis.sub.isReady ? "ok" : "error",
+      },
+    };
+  },
 });
--- server/api/quiz/list.get.ts
+++ server/api/quiz/list.get.ts
@@ -1,13 +1,19 @@
 import consola from "consola";
 import quizController from "~/controllers/quiz.controller";
 
-export default defineEventHandler(async (event) => {
-  try {
-    const list = await quizController.listQuizzes();
+export default defineEventHandler({
+  onRequest: [
+    cors({ credentials: true }),
+    authorization({ guest: "deny", user: "allow" }),
+  ],
+  handler: async (event) => {
+    try {
+      const list = await quizController.listQuizzes();
 
-    consola.info(`[Quiz] list ${list.length} quizzes`);
-    return list;
-  } catch (error) {
-    return sendError(event, error);
-  }
+      consola.info(`[Quiz] list ${list.length} quizzes`);
+      return list;
+    } catch (error) {
+      return sendError(event, error);
+    }
+  },
 });
